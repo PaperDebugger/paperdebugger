162f726fa9eef9f203416f9cd9823291785e88fb
Junyi Hou <hji200914@gmail.com>
2026-06-30 17:10:33 +0800

    add: tailwindcss + zustand

--- webapp/_webapp/wxt.config.ts
+++ webapp/_webapp/wxt.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'wxt';
-import { defineWebExtConfig } from 'wxt';
+import tailwindcss from '@tailwindcss/vite';
 // See https://wxt.dev/api/config.html
 
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
   webExt: {
     chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
   },
